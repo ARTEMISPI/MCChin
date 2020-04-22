735a3db82a8e5e791336fc859ff551f6bc3aab2b
Artemis <artemispapunidis@gmail.com>
2020-04-23 01:02:38 +0300

    Initial Commit

---
+++ MCChin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MCChin
+//
+//  Created by Artemis Papunidis on 4/23/20.
+//  Copyright © 2020 PAPUNIDIS DEVELOPMENT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
